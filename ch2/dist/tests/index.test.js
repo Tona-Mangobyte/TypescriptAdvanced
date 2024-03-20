aa492af03dd439940cfc1dbf107a0bc3d9f87a5c
Tona-Mangobyte <tona@mango-byte.com>
2024-03-20 12:02:36 +0700

    make simple

---
+++ ch2/dist/tests/index.test.js
@@ -0,0 +1,7 @@
+import assert from "assert";
+import { add } from "./math";
+describe("Adding numbers", () => {
+    it("should add two numbers", () => {
+        assert.equal(add(2, 3), 5);
+    });
+});
---
+++ ch2/src/index.ts
@@ -0,0 +1,5 @@
+import _ from "lodash"
+
+
+const result = _.flattenDeep([1, [2, [3, [4]], 5]]);
+console.log(result);
